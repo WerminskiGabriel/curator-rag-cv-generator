5f9fe6b09c4da573f18f74fdb7a21cdd987378af
Kamil Gieras <kamil.gieras@onet.pl>
2026-03-17 21:41:57 +0100

    Backend init

---
+++ Backend/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['POST'])
+def match(request):
+    jobs = request.data.get('jobs', [])
+    skills = request.data.get('skills', []) 
+    
+    job_store = {}
+    for text, job_id in jobs:
+        if True: # TODO decide to add or not add this to job_store 
+            job_store[job_id] = text
+    
+    return Response({'matched_ids': list(job_store.keys())})
