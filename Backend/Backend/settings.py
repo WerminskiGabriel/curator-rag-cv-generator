80846cb838cc6b386bb27f1492d48cb582cafe8e
WerminskiGabriel <gwerminski@student.agh.edu.pl>
2026-04-06 21:05:45 +0200

    settings fix

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -63,16 +63,20 @@ INSTALLED_APPS = [
     'frontend',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api.middleware.AutoLoginMiddleware',
 ]
 
 ROOT_URLCONF = 'Backend.urls'
